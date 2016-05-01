159f90e8d9127b9c19166d6509045dd9556e3480
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-05-01 22:20:16 +0200

    Add output token selection

--- parse.py
+++ parse.py
@@ -1,13 +1,12 @@
 import concraft
 import argparse
 
-parser = argparse.ArgumentParser()
-args = parser.add_argument("input", help="path to the input file", type=str)
-args = parser.add_argument("output", help="path to the output file", type=str)
-parser.parse_args()
-server = concraft.Server()
-port = server.get_port()
-client = concraft.Client(port)
-
-print(client.to_lemma(sentence))
-print(client.to_pos(sentence))
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    args = parser.add_argument("input", help="path to the input file", type=str)
+    args = parser.add_argument("output", help="path to the output file", type=str)
+    args = parser.add_argument("-t", "--tokens", help="type of the output tokens, use p for POS tags and l for lemmas (default)", type=str, choices='pl', default='l')
+    parser.parse_args()
+    server = concraft.Server()
+    port = server.get_port()
+    client = concraft.Client(port)
