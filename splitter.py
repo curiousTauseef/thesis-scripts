d7e3d740ff956581dea3a534a91d1bfcec96c839
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-05-02 16:42:13 +0200

    Rename some variables, improve help message

--- splitter.py
+++ splitter.py
@@ -3,9 +3,9 @@ import random
 
 def split(filename, treshold):
     treshold = max(treshold, 1-treshold)
-    with open(filename, 'r') as f:
+    with open(filename, 'r') as infile:
         with open(filename + '_train', 'w') as train, open(filename + '_test', 'w') as test:
-            for line in f:
+            for line in infile:
                 if random.random() > treshold:
                     test.write(line)
                 else:
@@ -14,6 +14,6 @@ def split(filename, treshold):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('input', help='path to the input file', type=str)
-    parser.add_argument('-t', '--treshold', help='numerical treshold for the split', type=float, default=0.8)
+    parser.add_argument('-t', '--treshold', help='between 0 and 1, the training set will always be the bigger one, i.e. 0.2 and 0.8 are equivalent', type=float, default=0.8)
     args = parser.parse_args()
     split(args.input, args.treshold)
