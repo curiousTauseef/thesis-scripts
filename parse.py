07e4f6eeaad03923df6d714a3e5344603845440b
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-05-02 12:49:42 +0200

    Fix argument parsing, add the main loop

--- parse.py
+++ parse.py
@@ -3,10 +3,14 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    args = parser.add_argument("input", help="path to the input file", type=str)
-    args = parser.add_argument("output", help="path to the output file", type=str)
-    args = parser.add_argument("-t", "--tokens", help="type of the output tokens, use p for POS tags and l for lemmas (default)", type=str, choices='pl', default='l')
-    parser.parse_args()
+    parser.add_argument('input', help='path to the input file', type=str)
+    parser.add_argument('output', help='path to the output file', type=str)
+    parser.add_argument('-t', '--tokens', help='type of the output tokens, use p for POS tags and l for lemmas (default)', type=str, choices='pl', default='l')
+    args = parser.parse_args()
     server = concraft.Server()
-    port = server.get_port()
-    client = concraft.Client(port)
+    client = concraft.Client(server.get_port())
+    mapping = {'l': client.to_lemmas, 'p': client.to_pos_tags}
+    with open(args.input, 'r') as infile, open(args.output, 'w') as out:
+        for line in infile:
+            print(line)
+            out.write(mapping[args.tokens](line))
