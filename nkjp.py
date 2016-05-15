a9e0276eff5503f8693aed8c055e8a3e31454686
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-05-15 13:23:16 +0200

    Organize output into sentences

--- nkjp.py
+++ nkjp.py
@@ -1,5 +1,9 @@
 import xml.etree.ElementTree
+
 root = xml.etree.ElementTree.parse('test.xml').getroot()
-for segment in root.findall('.//{http://www.tei-c.org/ns/1.0}seg'):
-    text = segment.find(".//{http://www.tei-c.org/ns/1.0}f[@name='orth']/{http://www.tei-c.org/ns/1.0}string")
-    print(text.text)
+for sentence in root.findall('.//{http://www.tei-c.org/ns/1.0}s'):
+    out = []
+    for segment in sentence.findall('.//{http://www.tei-c.org/ns/1.0}seg'): 
+        text = segment.find(".//{http://www.tei-c.org/ns/1.0}f[@name='orth']/{http://www.tei-c.org/ns/1.0}string")
+        out.append(text.text)
+    print(' '.join(out))
