6af504b64c5fec2020530ea97e17e244e53bb7db
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-05-19 00:02:51 +0200

    Extract other tags

--- nkjp.py
+++ nkjp.py
@@ -5,5 +5,7 @@ for sentence in root.findall('.//{http://www.tei-c.org/ns/1.0}s'):
     out = []
     for segment in sentence.findall('.//{http://www.tei-c.org/ns/1.0}seg'): 
         text = segment.find(".//{http://www.tei-c.org/ns/1.0}f[@name='orth']/{http://www.tei-c.org/ns/1.0}string")
-        out.append(text.text)
+        base = segment.find(".//{http://www.tei-c.org/ns/1.0}f[@name='base']/{http://www.tei-c.org/ns/1.0}string")
+        pos = segment.find(".//{http://www.tei-c.org/ns/1.0}f[@name='interpretation']/{http://www.tei-c.org/ns/1.0}string")
+        out.append(pos.text)
     print(' '.join(out))
