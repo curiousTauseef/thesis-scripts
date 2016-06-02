181449eac91f942ae0b4a2fe563cbdba312c3f46
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-06-02 22:27:44 +0200

    Separate POS from tags

--- parse.py
+++ parse.py
@@ -5,11 +5,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('input', help='path to the input file', type=str)
     parser.add_argument('output', help='path to the output file', type=str)
-    parser.add_argument('-t', '--tokens', help='type of the output tokens, use p for POS tags and l for lemmas (default)', type=str, choices='pl', default='l')
+    parser.add_argument('-t', '--tokens', help='type of the output tokens, use p for raw POS, t for POS with tags and l for lemmas (default)', type=str, choices='plt', default='l')
     args = parser.parse_args()
     with concraft.Server() as server:
         client = concraft.Client(server.get_port())
-        mapping = {'l': client.to_lemmas, 'p': client.to_pos_tags}
+        mapping = {'l': client.to_lemmas, 't': client.to_pos_tags, 'p': client.to_pos}
         with open(args.input, 'r') as infile, open(args.output, 'w') as out:
             for line in infile:
                 out.write(mapping[args.tokens](line) + "\n")
