65a7a4ab1f0e1ec2106ace8b2a7b2618e6ef6a4d
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-06-05 20:40:07 +0200

    Add the extraction of GNC tags, accept path as a command line argument

--- nkjp.py
+++ nkjp.py
@@ -4,10 +4,15 @@ import glob
 import os
 import re
 import string
+import sys
 
 prefix = '{http://www.tei-c.org/ns/1.0}'
 sentences = ".//{0}s".format(prefix)
 segments = ".//{0}seg".format(prefix)
+
+gender = ['m1', 'm2', 'm3', 'f', 'n']
+number = ['pl', 'sg']
+case = ['nom', 'gen', 'dat', 'acc', 'inst', 'loc', 'voc']
 
 def remove_nonalpha(string):
     pattern = re.compile('[\W_]+', re.UNICODE)
@@ -33,18 +38,27 @@ def extract_interpretation(segment):
     interpretation = ".//{0}f[@name='interpretation']/{0}string".format(prefix)
     return segment.find(interpretation).text.lower()
     
+def split_interpretation(interpretation):
+    interpretation = interpretation.split(':')
+    base = interpretation[0]
+    pos = interpretation[1]
+    gnc = extract_gnc(interpretation)
+
+def extract_gnc(interpretation):
+    pass
+
 def parse_sentence(sentence):
     parsed = []
     for segment in sentence.findall(segments):
         orth = extract_orthographic(segment)
-        interp =  extract_interpretation(segment).split(':')
-        base, pos = interp[0], interp[1]
+        interp =  extract_interpretation(segment)
+        base, pos, gnc = split_interpretation(interp)
         if is_num(orth):
             parsed.append('num')
-        elif pos == 'aglt':
-            parsed[-1] += orth
+        #elif pos == 'aglt':
+        #    parsed[-1] += pos
         elif pos not in ['brev', 'ign', 'interp', 'xxx']:
-            parsed.append(orth)
+            parsed.append(pos)
     return parsed
 
 def parse(xmlpath, out):
@@ -57,7 +71,7 @@ def parse(xmlpath, out):
                 print(parsed, file=out)
 
 if __name__ == '__main__':
-    path = '/media/sebastian/Seagate Expansion Drive/mgr/nkjp/misc/ustawy'
+    path = sys.argv[1]
     out= os.path.basename(os.path.normpath(path))
     open(out, 'w').close()
     xmlpath = os.path.join(path, '**/ann_morphosyntax.xml') 
