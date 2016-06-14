af11cea8f8ebdc0eda578e5b9b91702e331a2fb7
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-06-14 23:18:58 +0200

    Remove common functions

--- nkjp.py
+++ nkjp.py
@@ -10,25 +10,6 @@ prefix = '{http://www.tei-c.org/ns/1.0}'
 sentences = ".//{0}s".format(prefix)
 segments = ".//{0}seg".format(prefix)
 
-gender_tags = {'m1', 'm2', 'm3', 'f', 'n'}
-number_tags = {'pl', 'sg'}
-case_tags = {'nom', 'gen', 'dat', 'acc', 'inst', 'loc', 'voc'}
-
-def remove_nonalpha(string):
-    pattern = re.compile('[\W_]+', re.UNICODE)
-    return pattern.sub('', string)
-
-def contract_whitespace(string):
-    pattern = re.compile('\s+')
-    return pattern.sub(' ', string)
-
-def is_num(string):
-    roman = ['ii', 'iii', 'iv', 'v', 'vi', 'vii', 'viii', 'ix', 'x']
-    return string in roman or any(char.isdigit() for char in string)
-
-def is_valid(line):
-    return len(line) > 4 and line.count('num') < 3
-
 def extract_orthographic(segment):
     tag = ".//{0}f[@name='orth']/{0}string".format(prefix)
     orthographic = segment.find(tag).text.lower()
