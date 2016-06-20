d5018a60791a7721f3306fa826243b10005671c7
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-06-20 17:36:11 +0200

    Add a stub of a function generating phrases from a language model

--- srilm-wrapper.py
+++ srilm-wrapper.py
@@ -9,3 +9,14 @@ if __name__ == '__main__':
     with open('sentences') as sentences:
         for line in sentences:
             print("{0}: {1}".format(line.strip(), lm.total_logprob_strings(line.split())))
+
+    context = [lm.vocab.intern(w) for w in ["panie", "szanowny"]]
+    best_idx = None
+    best_logprob = -1e100
+    for i in xrange(lm.vocab.max_interned() + 1):
+        logprob = lm.logprob(i, context)
+        if logprob > best_logprob:
+            best_idx = i
+            best_logprob = logprob
+    best_word = lm.vocab.extern(best_idx)
+    print "Max prob continuation: %s (%s)" % (best_word, best_logprob)
