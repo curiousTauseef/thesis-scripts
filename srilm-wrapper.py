a6c2e4d001a613d4b75d5f17a83211a0e678e326
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-06-21 14:51:41 +0200

    Build one script for generating fake hypotheses

--- srilm-wrapper.py
+++ srilm-wrapper.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
     #parser.add_argument('path_to_lm', help='path to the language model file', type=str)
     #args = parser.parse_args()
     lm = LM('./text_plain_full_novocab.lm')
-    with open('sentences') as sentences:
+    with open('test_small_shuffled') as sentences:
         for line in sentences:
             print("{0}: {1}".format(line.strip(), lm.total_logprob_strings(line.split())))
 
