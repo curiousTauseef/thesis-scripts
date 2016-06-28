6e1ad2ae47d7a911bbd5dada34e41107483b3691
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-06-28 15:06:24 +0200

    Rename and reorder functions

--- fake_hypotheses.py
+++ fake_hypotheses.py
@@ -5,14 +5,26 @@ import time
 from utils import read_unigrams, read_hypotheses
 from collections import defaultdict
 
-def shuffled(line):
-    line = lines[0].strip().split()
+def generate_mock_hypotheses(hypo): 
+    for index, lines in hypo.items():
+        line = lines[0].strip().split()
+        for position, word in random.sample(list(enumerate(line)), n):
+            line[position] = substitute(word)
+        hypo[index].append(' '.join(line) + '\n')
+
+def shuffle(line):
+    line = line.strip().split()
     random.shuffle(line)
     return ' '.join(line) + '\n'
 
-def substituted(line): 
-    line = lines[0].strip().split()
-    n = len(line)//3 #substitute roughly one third of words
+def remove_words(line, percentage=0.3):
+    line = line.strip().split()
+    n = int(len(line)*(1-percentage))
+    return ' '.join([word for index, word in sorted(random.sample(list(enumerate(line)), n))])
+
+def substitute_words(line, percentage=0.3): 
+    line = line.strip().split()
+    n = int(len(line)*percentage)
     for position, word in random.sample(list(enumerate(line)), n):
         line[position] = substitute(word)
     return ' '.join(line) + '\n'
@@ -22,14 +34,6 @@ def substitute(word):
     similar = [word for distance, word in sorted(distance.ifast_comp(word, unigrams))][1:]
     return word if not similar else random.choice(similar)
 
-def substitute_words(hypo): 
-    for index, lines in hypo.items():
-        line = lines[0].strip().split()
-        n = len(line)//3 #substitute roughly one third of words
-        for position, word in random.sample(list(enumerate(line)), n):
-            line[position] = substitute(word)
-        hypo[index].append(' '.join(line) + '\n')
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('input', help='path to the input file', type=str)
@@ -38,8 +42,7 @@ if __name__ == '__main__':
     time_before = time.time()
     unigrams = read_unigrams('unigrams')
     hypo = read_hypotheses(args.input)
-    add_shuffled(hypo)
-    substitute_words(hypo)
+    generate_mock_hypotheses(hypo)
     write_hypotheses(args.input, hypo)
     time_after = time.time()
     print("Elapsed time: {0}".format(time_after-time_before))
