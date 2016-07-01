93fe47875d5418eff0e2821ff10484ab7dc9c8e3
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-07-01 11:35:44 +0200

    Wrap file read into a function

--- ngram_distributions.R
+++ ngram_distributions.R
@@ -1,8 +1,12 @@
 library(data.table)
 library(ggplot2)
-unigrams<-fread("data/results/ngrams/full_plain_unigrams", header=F, sep="\t")
-unigrams<-unigrams[order(count, decreasing=T),]
-colnames(unigrams)<-c("token", "count")
-unigrams$index<-seq(1:nrow(unigrams))
 
-qplot(log(unigrams$index), log(unigrams$count))+labs(x="log(n-gram index)", y="log(n-gram count)", title="Double logarithmic plot of the unigram occurrence distribution")
+myfunction <- function(filename){
+	ngrams<-fread(filename, header=F, sep="\t")
+	colnames(ngrams)<-c("token", "count")
+	ngrams<-ngrams[order(count, decreasing=T),]
+	ngrams$index<-seq(1:nrow(ngrams))
+	return(ngrams)
+}
+unigrams_plot = qplot(log(unigrams$index), log(unigrams$count))+labs(x="log(n-gram index)", y="log(n-gram count)", title="Double logarithmic plot of the unigram distribution for the full plain text corpus")
+ggsave(file="data/results/plots/unigrams_full.svg", plot=unigrams_plot, width=10, height=8)
