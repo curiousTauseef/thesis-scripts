ad6e5bedf80fd09fdb1d4555c6141a31c686ff66
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-07-01 13:39:10 +0200

    Wrap plot generation in a function

--- ngram_distributions.R
+++ ngram_distributions.R
@@ -1,12 +1,33 @@
 library(data.table)
+library(devtools)
 library(ggplot2)
 
-myfunction <- function(filename){
+source_url("https://raw.githubusercontent.com/ggrothendieck/gsubfn/master/R/list.R")
+
+read_ngram_file <- function(filename){
 	ngrams<-fread(filename, header=F, sep="\t")
 	colnames(ngrams)<-c("token", "count")
 	ngrams<-ngrams[order(count, decreasing=T),]
 	ngrams$index<-seq(1:nrow(ngrams))
 	return(ngrams)
 }
-unigrams_plot = qplot(log(unigrams$index), log(unigrams$count))+labs(x="log(n-gram index)", y="log(n-gram count)", title="Double logarithmic plot of the unigram distribution for the full plain text corpus")
-ggsave(file="data/results/plots/unigrams_full.svg", plot=unigrams_plot, width=10, height=8)
+
+read_ngrams <- function(path){
+	unigrams<-read_ngram_file(paste0(path, "_unigrams"))
+	bigrams<-read_ngram_file(paste0(path, "_bigrams"))
+	trigrams<-read_ngram_file(paste0(path, "_trigrams"))
+	return(c(unigrams, bigrams, trigrams))
+}
+
+path<-"data/results/ngrams/full_plain"
+list[unigrams, bigrams, trigrams]<-read_ngrams(path)
+cat(head(unigrams))
+cat(head(bigrams))
+cat(head(trigrams))
+unigram_plot = qplot(log(unigrams$index), log(unigrams$count))+labs(x="log(n-gram index)", y="log(n-gram count)", title="Double logarithmic plot of the unigram distribution for the full plain text corpus")
+bigram_plot = qplot(log(bigrams$index), log(bigrams$count))+labs(x="log(n-gram index)", y="log(n-gram count)", title="Double logarithmic plot of the bigram distribution for the full plain text corpus")
+trigram_plot = qplot(log(trigrams$index), log(trigrams$count))+labs(x="log(n-gram index)", y="log(n-gram count)", title="Double logarithmic plot of the trigram distribution for the full plain text corpus")
+
+ggsave(file="data/results/plots/unigrams_full.svg", plot=unigram_plot, width=10, height=8)
+ggsave(file="data/results/plots/bigrams_full.svg", plot=bigram_plot, width=10, height=8)
+ggsave(file="data/results/plots/trigrams_full.svg", plot=trigram_plot, width=10, height=8)
