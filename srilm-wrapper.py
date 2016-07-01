497ee531c3a44322c944c89079fb47aa20261531
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-07-01 11:35:20 +0200

    Add a command line argument

--- srilm-wrapper.py
+++ srilm-wrapper.py
@@ -2,10 +2,10 @@ from srilm import LM
 import argparse
 
 if __name__ == '__main__':
-    #parser = argparse.ArgumentParser()
-    #parser.add_argument('path_to_lm', help='path to the language model file', type=str)
-    #args = parser.parse_args()
-    lm = LM('./data/text_plain_full.lm')
-    with open('test_tiny_hypotheses') as sentences:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path', help='path to the language model file', type=str)
+    args = parser.parse_args()
+    lm = LM(args.path)#'./data/text_plain_full.lm')
+    with open('data/full/test') as sentences:
         for line in sentences:
             print("{0}: {1}".format(line.strip(), lm.total_logprob_strings(line.split())))
