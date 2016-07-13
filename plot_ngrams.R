505fed278132fa8e7f48d069c704e2a861c5e64a
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-07-13 16:00:48 +0200

    Use Cairo for anti-aliasing, leave first 1000 ngrams as they are

--- plot_ngrams.R
+++ plot_ngrams.R
@@ -1,8 +1,11 @@
 library(ggplot2)
+library(Cairo)
 library(grid)
 
-downsample <- function(ngrams, size=200000){
-	ngrams<-ngrams[sample(nrow(ngrams), size=size, replace=F),]
+downsample <- function(ngrams, size=200000, treshold=1000){
+	head<-head(ngrams, treshold)
+	tail<-ngrams[sample(treshold:nrow(ngrams), size=size, replace=F),]
+	ngrams<-rbind(head, tail)
 	ngrams<-ngrams[order(count, decreasing=T),]
 	return(ngrams)
 }
@@ -11,9 +14,9 @@ downsample <- function(ngrams, size=200000){
 unigrams_head = head(filter(unigrams, !grepl('num|ust|<s>|</s>', token)), 35)
 bigrams_head = head(filter(bigrams, !grepl('num|ust|<s>|</s>', token)), 35)
 trigrams_head = head(filter(trigrams, !grepl('num|ust|<s>|</s>', token)), 35)
-unigrams = downsample(unigrams)
-bigrams = downsample(bigrams)
-trigrams = downsample(trigrams)
+unigrams_downsampled = downsample(unigrams)
+bigrams_downsampled = downsample(bigrams)
+trigrams_downsampled = downsample(trigrams)
 
 ##plot most common tokens
 #common_unigrams = qplot(unigrams_head$token, unigrams_head$count/sum(unigrams$count), geom="bar") +
@@ -38,21 +41,21 @@ trigrams = downsample(trigrams)
 ##theme(axis.title.x=element_text(margin=margin(0,20,0,0)))
 
 unigram_plot = ggplot(unigrams_downsampled, aes(x=log(index), y=log(count))) +
-geom_point(size=0.3) +
+geom_point(size=0.5) +
 labs(x="log(n-gram index)", y="log(n-gram count)")
 
 bigram_plot = ggplot(bigrams_downsampled, aes(x=log(index), y=log(count))) +
-geom_point(size=0.3) +
+geom_point(size=0.5) +
 labs(x="log(n-gram index)", y="log(n-gram count)")
 
 trigram_plot = ggplot(trigrams_downsampled, aes(x=log(index), y=log(count))) +
-geom_point(size=0.3) +
+geom_point(size=0.5) +
 labs(x="log(n-gram index)", y="log(n-gram count)")
 
 #save files
-ggsave(file=paste0("data/results/plots/distributions/", corpus_name, "_unigrams.svg"), plot=unigram_plot, width=10, height=8)
-ggsave(file=paste0("data/results/plots/distributions/", corpus_name, "_bigrams.svg"), plot=bigram_plot, width=10, height=8)
-ggsave(file=paste0("data/results/plots/distributions/", corpus_name, "_trigrams.svg"), plot=trigram_plot, width=10, height=8)
+ggsave(file=paste0("data/results/plots/distributions/", corpus_name, "_unigrams.png"), plot=unigram_plot, width=15, height=10, dpi=600, type='cairo-png')
+ggsave(file=paste0("data/results/plots/distributions/", corpus_name, "_bigrams.png"), plot=bigram_plot, width=15, height=10, dpi=600, type='cairo-png')
+ggsave(file=paste0("data/results/plots/distributions/", corpus_name, "_trigrams.png"), plot=trigram_plot, width=15, height=10, dpi=600, type='cairo-png')
 #ggsave(file=paste0("data/results/plots/common/", corpus_name, ".svg"), plot=common_unigrams, width=10, height=8)
 #ggsave(file=paste0("data/results/plots/common/", corpus_name, ".svg"), plot=common_bigrams, width=10, height=8)
 #ggsave(file=paste0("data/results/plots/common/", corpus_name, ".svg"), plot=common_trigrams, width=10, height=8)
