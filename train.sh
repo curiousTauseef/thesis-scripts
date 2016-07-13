025323826ca4203dd6aba9841daadd95e7796b89
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-07-14 00:20:09 +0200

    Final

--- train.sh
+++ train.sh
@@ -6,15 +6,64 @@ LM=/home/sebastian/Dokumenty/IS/Thesis/language-models/scripts/data/lm
 $SRILM/ngram-count \
 -order 3 \
 -text "$DATA/full/$1" \
--lm "$DATA/lm/$1.lm" \
--kndiscount1 -kndiscount2 -kndiscount3 \
--interpolate1 -interpolate2 -interpolate3 \
--write1 "$DATA/results/ngrams/$1_unigrams" \
--write2 "$DATA/results/ngrams/$1_bigrams" \
--write3 "$DATA/results/ngrams/$1_trigrams" \
+-lm "$DATA/lm/$1_gt.lm" \
+-gt3min 0 \
 -unk
 
-echo "$1:"
-$SRILM/ngram -ppl $DATA/test/test -order 1 -lm "$DATA/lm/$1.lm" -unk -debug 0
-$SRILM/ngram -ppl $DATA/test/test -order 2 -lm "$DATA/lm/$1.lm" -unk -debug 0
-$SRILM/ngram -ppl $DATA/test/test -order 3 -lm "$DATA/lm/$1.lm" -unk -debug 0
+$SRILM/ngram-count \
+-order 3 \
+-text "$DATA/full/$1" \
+-lm "$DATA/lm/$1_kn.lm" \
+-gt3min 0 \
+-unk \
+-ukndiscount1 -ukndiscount2 -ukndiscount3
+
+$SRILM/ngram-count \
+-order 3 \
+-text "$DATA/full/$1" \
+-lm "$DATA/lm/$1_kni.lm" \
+-gt3min 0 \
+-unk \
+-ukndiscount1 -ukndiscount2 -ukndiscount3 \
+-interpolate1 -interpolate2 -interpolate3
+
+$SRILM/ngram-count \
+-order 3 \
+-text "$DATA/full/$1" \
+-lm "$DATA/lm/$1_cg.lm" \
+-gt3min 0 \
+-unk \
+-kndiscount1 -kndiscount2 -kndiscount3 \
+
+$SRILM/ngram-count \
+-order 3 \
+-text "$DATA/full/$1" \
+-lm "$DATA/lm/$1.lm" \
+-gt3min 0 \
+-unk \
+-kndiscount1 -kndiscount2 -kndiscount3 \
+-interpolate1 -interpolate2 -interpolate3
+#-write1 "$DATA/results/ngrams/$1_unigrams" \
+#-write2 "$DATA/results/ngrams/$1_bigrams" \
+#-write3 "$DATA/results/ngrams/$1_trigrams" \
+
+echo "Good-Turing"
+$SRILM/ngram -ppl $DATA/test/test -order 1 -lm "$DATA/lm/$1_gt.lm" -unk -debug 0
+$SRILM/ngram -ppl $DATA/test/test -order 2 -lm "$DATA/lm/$1_gt.lm" -unk -debug 0
+$SRILM/ngram -ppl $DATA/test/test -order 3 -lm "$DATA/lm/$1_gt.lm" -unk -debug 0
+echo "Kneser-Ney"
+$SRILM/ngram -ppl $DATA/test/test -order 1 -lm "$DATA/lm/$1_kn.lm" -unk -debug 0
+$SRILM/ngram -ppl $DATA/test/test -order 2 -lm "$DATA/lm/$1_kn.lm" -unk -debug 0
+$SRILM/ngram -ppl $DATA/test/test -order 3 -lm "$DATA/lm/$1_kn.lm" -unk -debug 0
+echo "Kneser-Ney Interpolated"
+$SRILM/ngram -ppl $DATA/test/test -order 1 -lm "$DATA/lm/$1_kni.lm" -unk -debug 0
+$SRILM/ngram -ppl $DATA/test/test -order 2 -lm "$DATA/lm/$1_kni.lm" -unk -debug 0
+$SRILM/ngram -ppl $DATA/test/test -order 3 -lm "$DATA/lm/$1_kni.lm" -unk -debug 0
+echo "Chen-Goodman"
+$SRILM/ngram -ppl $DATA/test/test -order 1 -lm "$DATA/lm/$1_cg.lm" -unk -debug 0
+$SRILM/ngram -ppl $DATA/test/test -order 2 -lm "$DATA/lm/$1_cg.lm" -unk -debug 0
+$SRILM/ngram -ppl $DATA/test/test -order 3 -lm "$DATA/lm/$1_cg.lm" -unk -debug 0
+echo "Chen-Goodman Interpolated"
+$SRILM/ngram -ppl $DATA/test/test -order 1 -lm "$DATA/lm/$1_cgi.lm" -unk -debug 0
+$SRILM/ngram -ppl $DATA/test/test -order 2 -lm "$DATA/lm/$1_cgi.lm" -unk -debug 0
+$SRILM/ngram -ppl $DATA/test/test -order 3 -lm "$DATA/lm/$1_cgi.lm" -unk -debug 0
