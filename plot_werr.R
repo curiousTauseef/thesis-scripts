5e7d15685a1d97775996c74b670b76120f13596f
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-07-15 00:17:34 +0200

    Change export format to PNG:

--- plot_werr.R
+++ plot_werr.R
@@ -1,13 +1,15 @@
 library(data.table)
+library(Cairo)
 library(ggplot2)
 
 corpus_name="speech_plain"
 
 data = fread(paste0("data/results/werr/", corpus_name, ".werr"))
 colnames(data) = c("alpha", "WERR")
-plot = qplot(data$alpha, data$WERR, geom="line") + 
+
+werr = ggplot(data, aes(x=alpha, y=WERR, geom="line")) +
 labs(x="alpha", y="WERR") +
 theme(axis.title.y=element_text(margin=margin(0, 0, 0, 0))) +
-theme(axis.title.x=element_text(margin=margin(0, 0, 0, 0))) +
+theme(axis.title.x=element_text(margin=margin(0, 0, 0, 0)))
 
-ggsave(file=paste0("data/results/plots/werr/", corpus_name, "_werr.svg"), plot=plot, width=10, height=8)
+ggsave(file=paste0("data/results/plots/werr/", corpus_name, "_werr.png"), plot=werr, width=15, height=12, dpi=600, type='cairo-png')
