ab791e221fbec38605074028fb290a59ae040310
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-07-17 02:05:50 +0200

    Fix wrong names, fix a bug with a generator expression

--- parse_acoustic.py
+++ parse_acoustic.py
@@ -9,20 +9,20 @@ def tag(line, function, eos):
     line.rstrip()
     if eos:
         line = re.sub(r'|'.join(map(re.escape, ['<s> ', ' </s>'])), '', line)
-    mapped = function(line).encode('UTF-8')
+    mapped = function(line)
     return "<s> {} </s>\n".format(mapped) if eos else mapped + '\n'
 
 def tag_recursively(directory, function, eos): 
     for filename in glob.iglob(os.path.join(args.input, '**/acoustic_hypotheses.txt'), recursive=True):
         with open(filename, 'r') as f:
-            lines = (tag(line, function, eos) for line in f)
+            lines = [tag(line, function, eos) for line in f]
         with open(filename, 'w') as out:
             out.write('\n'.join(lines) + '\n')
 
-def tag(infile, outfile, function, eos): 
+def tag_file(infile, outfile, function, eos): 
     with open(infile, 'r') as f, open(outfile, 'w') as out:
         for line in f:
-            out.write(tag_line(line, function, eos))
+            out.write(tag(line, function, eos))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -31,15 +31,15 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--tokens', help='type of the output tokens, use p for raw POS, t for POS with tags and l for lemmas (default)', type=str, choices='gplt', default='l')
     parser.add_argument('--eos', dest='eos', action='store_true')
     parser.add_argument('--no-eos', dest='eos', action='store_false')
-    parser.add_argument('--recursive', dest='eos', action='store_true')
-    parser.add_argument('--not-recursive', dest='eos', action='store_false')
-    parser.set_defaults(feature=True)
+    parser.add_argument('--recursive', dest='recursive', action='store_true')
+    parser.add_argument('--not-recursive', dest='recursive', action='store_false')
+    parser.set_defaults(eos=False)
+    parser.set_defaults(recursive=False)
     args = parser.parse_args()
-    function = mapping[args.tokens]
     with concraft.Server() as server:
         client = concraft.Client(server.get_port())
-        function = {'l': client.to_lemmas, 't': client.to_pos_tags, 'p': client.to_pos, 'g': client.to_gnc}[tokens]
+        function = {'l': client.to_lemmas, 't': client.to_pos_tags, 'p': client.to_pos, 'g': client.to_gnc}[args.tokens]
         if args.recursive:
             tag_recursively(args.input, function , args.eos)
         else:
-            tag(args.input, args.output, function, eos)
+            tag_file(args.input, args.output, function, args.eos)
