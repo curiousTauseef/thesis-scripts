26c615d6ac4947908f688dd42336d864a2fc9b03
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-07-21 01:04:59 +0200

    Move functions from utils

--- fake_hypotheses.py
+++ fake_hypotheses.py
@@ -7,7 +7,7 @@ from utils import write_hypotheses, read_unigrams, read_hypotheses
 def append_mocks(hypotheses): 
     for index, lines in hypotheses.items():
         line = lines[0].strip().split()
-        hypotheses[index].extend((reduce(mock) for mock in (substitute_words(line), shuffle(line), remove_words(line))))
+        hypotheses[index].extend((reduce(mock) for mock in (substitute_words(line, 0.1), substitute_words(line, 0.3), substitue_words(line, 0.7), shuffle(line), remove_words(line))))
 
 def reduce(mock):
     return ' '.join(mock) + '\n'
@@ -26,8 +26,26 @@ def substitute(word):
     similar = [word for distance, word in sorted(distance.ifast_comp(word, unigrams)) if distance > 0]
     return word if not similar else random.choice(similar)
 
+def write_hypotheses(filename, hypo):
+    with open(filename + '_hypotheses', 'w') as out:
+        for index in sorted(hypo):
+            for line in hypo[index]:
+                out.write("{0} {1}".format(index, line))
+
+def read_hypotheses(filename):
+    hypo = defaultdict(list)
+    with open(filename, 'r') as f:
+        for index, line in enumerate(f):
+            hypo[index+1].append(line)
+    return hypo
+
+def read_unigrams(filename):
+    with open(filename, 'r') as f:
+        unigrams = [line.split()[0] for line in f]
+    return unigrams
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.argumentparser()
     parser.add_argument('input', help='path to the input file', type=str)
     args = parser.parse_args()
 
