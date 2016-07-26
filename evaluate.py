44277b7bf557cb615ff0ccca3ae64fb89e51c8df
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-07-26 19:37:54 +0200

    Rename variables, start extracting some code into functions

--- evaluate.py
+++ evaluate.py
@@ -4,30 +4,36 @@ import argparse
 from collections import defaultdict
 from srilm import LM
 
-def read_hypotheses(filename):
-    hypo = defaultdict(list)
+def read_nbest_list(filename):
+    nbest = defaultdict(list)
     with open(filename, 'r') as f:
         for line in f:
             index, text = line.split("\t")
-            hypo[int(index)].append(text)
-    return hypo
+            nbest[int(index)].append(text)
+    return nbest
 
-def get_prob(lm):
-    def calculate_prob(hypothesis)
-        return lm.total_logprob_strings(hypothesis.split())
+def get_prob_fun(lm):
+    def calculate_prob(hypothesis):
+        return lm.total_logprob_strings([word.encode('utf-8') for word in hypothesis.split()])
     return calculate_prob
+
+def calculate_werr(reference, hypothesis):
+    pass
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('path', help='path to the language model file', type=str)
     parser.add_argument('test', help='path to the test file', type=str)
     args = parser.parse_args()
-    lm = LM(args.path)
+    lm = LM(args.path.encode('utf-8'))
     score = get_prob_fun(lm)
-    hypo = read_hypotheses(args.test)
+    nbest = read_nbest_list(args.test)
     werr_total = 0
-    for index in hypo:
-        reference = hypo[index][0]
-        scores = [(hypothesis, score(hypothesis)) for hypothesis in hypo[index][1:]] 
+    for index in nbest:
+        reference = nbest[index][0]
+        hypotheses = nbest[index][1:]
+        scores = [(hypothesis, score(hypothesis)) for hypothesis in hypotheses] 
         best = max(scores, key=operator.itemgetter(1))
-        werr_total +=  distance.levenshtein(reference.split(), best.split())
+        distance = distance.levenshtein(reference.split(), best.split()) 
+        werr_total +=  distance/len(reference)
+    print(werr_total/len(hypo))
