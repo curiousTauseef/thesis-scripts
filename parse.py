4c65488e28337ad8082b875e706584449e296d89
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-07-30 01:14:05 +0200

    Rename hypothesis to n-best list

--- parse.py
+++ parse.py
@@ -15,7 +15,7 @@ def strip(line, eos):
 def add_eos(line):
     return "<s> {} </s>".format(line) 
 
-def tag(line, function, eos=False):
+def tag(line, function, eos):
     line = strip(line, eos)
     tagged = function(line)
     return add_eos(tagged) if eos else tagged
@@ -28,16 +28,16 @@ def tag_recursively(directory, function, eos):
         with open(filename, 'w') as out:
             out.write('\n'.join(lines) + '\n')
 
-def tag_hypotheses(infile, outfile, function): 
+def tag_nbest(infile, outfile, function): 
     with open(infile, 'r') as f, open(outfile, 'w') as out:
         for line in f:
-            out.write(tag(line, function, eos))
+            index, text = line.split("\t")
+            out.write(index, tag(text, function, eos))
 
 def tag_file(infile, outfile, function, eos): 
     with open(infile, 'r') as f, open(outfile, 'w') as out:
         for line in f:
-            index, text = line.split("\t")
-            out.write(index, tag(line, function))
+            out.write(tag(line, function, eos))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     parser.add_argument('-t', '--tokens', help='type of the output tokens, use p for raw POS, t for POS with tags and l for lemmas (default)', type=str, choices='gplt', default='l')
     parser.add_argument('--eos', dest='eos', action='store_true')
     parser.add_argument('--no-eos', dest='eos', action='store_false')
-    parser.add_argument('-f' '--file', help='type of the file, use c for corpora, f for single file, and h for hypothesis file', type=str, choces='cfh', default='f')
+    parser.add_argument('-f' '--file', help='type of the file, use c for corpora, f for single file, and n for nbest file', type=str, choces='cfh', default='f')
     parser.set_defaults(eos=False)
     parser.set_defaults(recursive=False)
     args = parser.parse_args()
@@ -55,7 +55,7 @@ if __name__ == '__main__':
         function = {'l': client.to_lemmas, 't': client.to_pos_tags, 'p': client.to_pos, 'g': client.to_gnc}[args.tokens]
         if args.type == c:
             tag_recursively(args.input, function , args.eos)
-        elif args.type == h:
-            tag_hypotheses(args.input, args.output, function)
+        elif args.type == n:
+            tag_nbest(args.input, args.output, function)
         else:
             tag_file(args.input, args.output, function, args.eos)
