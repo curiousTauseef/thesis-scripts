87358ea488cca7d1c9acf835f4d285b9b97a7dae
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-07-31 22:59:04 +0200

    A script for scoring n-best lists using an n-gram model

---
+++ score_nbest.py
@@ -0,0 +1,20 @@
+import argparse
+from srilm import LM
+
+def get_score_function(lm):
+    def score_function(hypothesis):
+        return lm.total_logprob_strings(hypothesis.strip().split())
+    return score_function
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('lm', help='path to the language model file', type=str)
+    parser.add_argument('out', help='path to the output file', type=str)
+    parser.add_argument('nbest', help='path to the nbest list file', type=str)
+    args = parser.parse_args()
+    lm = LM(args.lm.encode('utf-8'))
+    score = get_score_function(lm)
+    with open(args.nbest, 'r') as f, open(args.out, 'w') as out:
+        for line in f:
+            index, text = line.strip().split("\t")
+            out.write(score(text))
