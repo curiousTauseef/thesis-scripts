f5cea4dd68dd64e6bfcc8cbb7500a016b0d46a4e
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-08-13 19:03:56 +0200

    Fix bug

--- extract_hypotheses.py
+++ extract_hypotheses.py
@@ -11,13 +11,16 @@ if __name__ == '__main__':
     parser.add_argument('output', help='path to the output file', type=str)
     args = parser.parse_args()
     with open(os.path.join(args.input, 'metadata.yml'), 'r') as metadata:
-            references = [elem[':answer'] for elem in yaml.load(metadata)]
-    subdirectories = natsorted([subdir[0] for subdir in os.walk(args.input)][1:]) 
+        doc = yaml.load(metadata)
+    subdirectories = sorted([subdir[0] for subdir in os.walk(args.input)][1:]) 
     for index, folder in enumerate(subdirectories):
+        for elem in doc:
+            if elem[':path'] == os.path.basename(folder):
+                reference = elem[':answer']
         filename = os.path.join(folder, 'acoustic_hypotheses.txt')
         print(filename)
         with open(filename, 'r') as f, open(args.output, 'a') as out:
-            out.write("{0} {1}\n".format(index+1, references[index]))
+            out.write("{0}\t{1}\n".format(index+1, reference))
             for line in f:
                 line = line.split()[1:-1]
-                out.write("{0} {1}\n".format(index+1, ' '.join(line)))
+                out.write("{0}\t{1}\n".format(index+1, ' '.join(line)))
